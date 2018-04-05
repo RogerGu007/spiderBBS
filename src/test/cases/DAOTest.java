c4d89e6482913375dd8dcda728990ead33b8dc8d
tjshif <Welcome@1>
2018-04-05 13:14:19 +0800

    add unit test case folder

--- src/test/cases/DAOTest.java
+++ src/test/cases/DAOTest.java
@@ -1,4 +1,4 @@
-package com.school.cases;
+package cases;
 
 import com.school.dao.INewsDAO;
 import com.school.entity.News;
@@ -10,7 +10,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+
+@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class DAOTest {
 
