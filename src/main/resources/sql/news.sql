f13e7dd52fe25b8606346a42f38730a53943ae8b
tjshif <37956713+tjshif@users.noreply.github.com>
2018-04-09 23:20:52 +0800

    add news.sql

--- src/main/resources/sql/news.sql
+++ src/main/resources/sql/news.sql
@@ -18,8 +18,8 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for `news`
 -- ----------------------------
-DROP TABLE IF EXISTS `news`;
-CREATE TABLE `news` (
+DROP TABLE IF EXISTS `News`;
+CREATE TABLE `News` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Subject` varchar(512) NOT NULL,
   `NewsType` tinyint(4) NOT NULL,
@@ -27,17 +27,16 @@ CREATE TABLE `news` (
   `PostDate` datetime DEFAULT NULL,
   `LocationCode` tinyint(4) DEFAULT NULL,
   `isHot` tinyint(4) DEFAULT NULL,
-  `CreateAt` datetime NOT NULL,
-  `CreateBy` varchar(45) CHARACTER SET latin1 NOT NULL,
   `linkUrl` varchar(512) DEFAULT NULL,
+  `CreateAt` datetime NOT NULL,
+  `CreateBy` varchar(45) DEFAULT NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `IDX_LINKURL` (`linkUrl`) USING BTREE,
   KEY `UK_SUBJECT` (`Subject`),
   KEY `IDX_TYPE_SUBTYPE` (`NewsType`,`NewsSubType`),
   KEY `IDX_CREATEDATE` (`CreateAt`),
   KEY `IDX_LOCATION` (`LocationCode`)
-) ENGINE=InnoDB AUTO_INCREMENT=1963 DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='News info';
 -- ----------------------------
 -- Records of news
 -- ----------------------------
