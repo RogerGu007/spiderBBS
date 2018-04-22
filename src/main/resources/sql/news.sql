99a2e368e4e2296bb2332d22fa2d70898fddf3fd
RogerGu007 <rogergu008@163.com>
2018-04-22 12:26:55 +0800

    db变更：news表添加isValid字段

--- src/main/resources/sql/news.sql
+++ src/main/resources/sql/news.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2018-04-06 14:59:02
+Date: 2018-04-22 12:25:37
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -18,28 +18,24 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for `news`
 -- ----------------------------
-DROP TABLE IF EXISTS `News`;
-CREATE TABLE `News` (
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Subject` varchar(512) NOT NULL,
   `NewsType` tinyint(4) NOT NULL,
   `NewsSubType` tinyint(4) DEFAULT NULL,
   `PostDate` datetime DEFAULT NULL,
-  `LocationCode` tinyint(4) DEFAULT NULL,
+  `LocationCode` int(4) DEFAULT NULL,
   `isHot` tinyint(4) DEFAULT NULL,
-  `linkUrl` varchar(512) DEFAULT NULL,
   `CreateAt` datetime NOT NULL,
-  `CreateBy` varchar(45) NOT NULL,
-  `publisherId` int(10) unsigned NOT NULL,
+  `CreateBy` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `linkUrl` varchar(512) DEFAULT NULL,
+  `publisherId` int(8) DEFAULT NULL,
+  `isValid` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`ID`),
   UNIQUE KEY `IDX_LINKURL` (`linkUrl`) USING BTREE,
   KEY `UK_SUBJECT` (`Subject`),
   KEY `IDX_TYPE_SUBTYPE` (`NewsType`,`NewsSubType`),
   KEY `IDX_CREATEDATE` (`CreateAt`),
-  KEY `IDX_LOCATION` (`LocationCode`),
-  CONSTRAINT `FK_PublishID` FOREIGN KEY (`publisherId`) REFERENCES `User` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='News info'
-
--- ----------------------------
--- Records of news
--- ----------------------------
+  KEY `IDX_LOCATION` (`LocationCode`)
+) ENGINE=InnoDB AUTO_INCREMENT=3224 DEFAULT CHARSET=utf8;
